0fb5e00165cc0cef28be854123f420ea4e1d0bd9
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-12-06 14:02:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,22 +9,20 @@ setInterval(function() {
 channel.send(`Fuck You`);
 }, 30)
 })
-client.on('message', message => {
-        var prefix = "!";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-
-
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-
-
-      });
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "tell") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
 client.login(process.env.BOT_TOKEN); 
